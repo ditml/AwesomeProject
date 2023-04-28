646522bbd112ab6e8313f43627643e6916bc4ef9
durgaprasad <durgaprasad.myway.s@gmail.com>
2023-04-28 17:56:44 +0530

    Enable/Disable React Freeze feature of React Navigation

--- index.js
+++ index.js
@@ -6,4 +6,8 @@ import {AppRegistry} from 'react-native';
 import App from './App';
 import {name as appName} from './app.json';
 
+import {enableFreeze} from 'react-native-screens'; // React Freeze
+
+enableFreeze(true); // Enable - 'true | Disable - false | React Freeze
+
 AppRegistry.registerComponent(appName, () => App);
