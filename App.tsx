19ac328c01763d09b793c798817dcbdf75082408
durgaprasad <durgaprasad.myway.s@gmail.com>
2023-04-28 19:14:19 +0530

    Add React-Native-SVG example

--- App.tsx
+++ App.tsx
@@ -1,61 +1,11 @@
 import React from 'react';
 import {View, Text, StyleSheet} from 'react-native';
 import {Icon} from '@rneui/themed';
-import Svg, {
-  Circle,
-  Ellipse,
-  G,
-  TSpan,
-  TextPath,
-  Path,
-  Polygon,
-  Polyline,
-  Line,
-  Rect,
-  Use,
-  Image,
-  Symbol,
-  Defs,
-  LinearGradient,
-  RadialGradient,
-  Stop,
-  ClipPath,
-  Pattern,
-  Mask,
-} from 'react-native-svg';
+import Svg, {Circle, Rect} from 'react-native-svg';
 
 export default () => {
   return (
     <>
-      <View
-        style={{
-          alignItems: 'center',
-          paddingVertical: 5,
-          flexGrow: 1,
-        }}>
-        <Text>
-          <Icon name="rowing" />
-
-          <Icon name="g-translate" color="#00aced" />
-
-          <Icon name="sc-telegram" type="evilicon" color="#517fa4" />
-
-          <Icon
-            reverse
-            name="ios-american-football"
-            type="ionicon"
-            color="#517fa4"
-          />
-
-          <Icon
-            raised
-            name="heartbeat"
-            type="font-awesome"
-            color="#f50"
-            onPress={() => console.log('hello')}
-          />
-        </Text>
-      </View>
       <View
         style={[
           StyleSheet.absoluteFill,
