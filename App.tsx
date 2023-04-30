c1d6c7d476e235735c58e01303ef6624b5cce61b
durgaprasad <durgaprasad.myway.s@gmail.com>
2023-04-30 15:11:34 +0530

    Add Folder - /src/foo-eg/foo-eg-xx.tsx _ Project Structure

--- App.tsx
+++ App.tsx
@@ -1,36 +1,6 @@
 import React from 'react';
-import {View, Text, StyleSheet} from 'react-native';
-import {Icon} from '@rneui/themed';
-import Svg, {Circle, Rect} from 'react-native-svg';
+import Eg from './src/Eg/Eg';
 
-export default () => {
-  return (
-    <>
-      <View
-        style={[
-          StyleSheet.absoluteFill,
-          {alignItems: 'center', justifyContent: 'center'},
-        ]}>
-        <Svg height="50%" width="50%" viewBox="0 0 100 100">
-          <Circle
-            cx="50"
-            cy="50"
-            r="45"
-            stroke="blue"
-            strokeWidth="2.5"
-            fill="green"
-          />
-          <Rect
-            x="15"
-            y="15"
-            width="70"
-            height="70"
-            stroke="red"
-            strokeWidth="2"
-            fill="yellow"
-          />
-        </Svg>
-      </View>
-    </>
-  );
-};
+export default function App() {
+  return <Eg />;
+}
